d02fa58be83ab1f89a309e433db444d1793b7d02
Sam-GTECH <sburton@gaming.tech>
2024-03-21 19:03:51 +0100

    oops

--- PokemonConsole/Game.cs
+++ PokemonConsole/Game.cs
@@ -99,7 +99,7 @@ namespace PokemonConsole
                 Console.Write("═");
             }
             Console.WriteLine("╗");
-            for (int i = _size - 1; i >= 0; i--)
+            for (int i = 0; i < _size - 1; i++)
             {
                 Console.Write("║");
                 for (int j = 0; j < _size; j++)
